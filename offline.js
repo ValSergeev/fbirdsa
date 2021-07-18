0f23eb163fe78fae6a4499b6b461b01f601b8484
Val <ValSergeev@users.noreply.github.com>
2021-07-19 00:42:51 +0200

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1626647015,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sky.png",
+		"images/pipe-sheet0.png",
+		"images/bird-sheet0.png",
+		"images/score.png",
+		"images/ground.png",
+		"images/dirt-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
